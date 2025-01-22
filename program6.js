11479e4f63cf8c3d2a54204bbede0301aa845c3f
Abhinav Joshi <joshiexzz.11@gmail.com>
2025-01-22 19:39:00 +0545

    Delete program6.js

--- program6.js
+++
@@ -1,7 +0,0 @@
-let numbers = [1, 3, 7, 2, 5,8,33,22,64,111];
-const findMax=(numbers)=>{
-    return Math.max(...numbers);
-  }
-  let maxNumber = findMax(numbers);
-  console.log("The maximum number is:", maxNumber);
-  
