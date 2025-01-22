18bf80a75e88d4e7a3b727c9f3b349c75a3547f6
Abhinav Joshi <joshiexzz.11@gmail.com>
2025-01-22 19:39:15 +0545

    Delete program8.js

--- program8.js
+++
@@ -1,9 +0,0 @@
-//Traditional Function
-/*function getSquare(num){
-    return num*num
-}
-console.log(getSquare(2))*/
-
-//Arrow Function
-const getSquare=(num)=>{return num*num}
-console.log(getSquare(2))
